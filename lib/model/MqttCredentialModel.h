8c5162a65b5004e25967f09f36bec31459a59b6f
berat dinckan <bedinckan@gmail.com>
2024-01-28 13:09:34 +0300

    Add MqttCredentialModel class definition

---
+++ lib/model/MqttCredentialModel.h
@@ -0,0 +1,25 @@
+// MqttCredentialModel class definition.
+
+#ifndef MQTTCREDENTIALMODEL_H
+#define MQTTCREDENTIALMODEL_H
+
+#include <Arduino.h>
+
+class MqttCredentialModel
+{
+public:
+    int port;
+    String host;
+    String clientId;
+    String publishTopic;
+
+    MqttCredentialModel() : port(0), host(""), clientId(""), publishTopic(""){};
+    MqttCredentialModel(int port, String host, String clientId, String publishTopic) : port(port), host(host), clientId(clientId), publishTopic(publishTopic){};
+
+    bool isEmpty()
+    {
+        return port == 0 || host == "" || clientId == "" || publishTopic == "";
+    }
+};
+
+#endif
