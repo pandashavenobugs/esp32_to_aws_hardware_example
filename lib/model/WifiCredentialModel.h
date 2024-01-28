8702e1a7fdf857d37e07491798f6e90da09ba617
berat dinckan <bedinckan@gmail.com>
2024-01-28 13:09:37 +0300

    Add WifiCredentialModel class definition

---
+++ lib/model/WifiCredentialModel.h
@@ -0,0 +1,22 @@
+// WifiCredentialModel class definition.
+
+#ifndef WIFICREDENTIALMODEL_H
+#define WIFICREDENTIALMODEL_H
+
+#include <Arduino.h>
+
+class WifiCredentialModel
+{
+public:
+    String ssid;
+    String password;
+    WifiCredentialModel() : ssid(""), password(""){};
+    WifiCredentialModel(String ssid, String password) : ssid(ssid), password(password){};
+
+    bool isEmpty()
+    {
+        return ssid == "" || password == "";
+    }
+};
+
+#endif
